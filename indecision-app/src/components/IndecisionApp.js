5c8fc555f15164a65720a5a45ca8499b0c2b0d54
Denallix Administrator <fukszon.kostya@gmail.com>
2018-03-14 15:21:26 -0700

    Added CSS and styles

--- indecision-app/src/components/IndecisionApp.js
+++ indecision-app/src/components/IndecisionApp.js
@@ -66,18 +66,22 @@ export default class IndecisionApp extends React.Component{
             <div>
                 <Header
                     subtitle={subtitle}/>
-                <Action
-                    hasOptions={this.state.options.length>0}
-                    handlePick={this.handlePick}/>
-                <Options 
-                    options={this.state.options}
-                    handleDeleteOptions={this.handleDeleteOptions}
-                    handleDeleteOption={this.handleDeleteOption}/>
-                <AddOption 
-                    handleAddOption={this.handleAddOption} />
-                <OptionModal 
-                    selectedOption={this.state.selectedOption}
-                    handleClearSelectedOption = {this.handleClearSelectedOption}/>
+                <div className="container">
+                    <Action
+                        hasOptions={this.state.options.length>0}
+                        handlePick={this.handlePick}/>
+                    <div className="widget">
+                        <Options 
+                            options={this.state.options}
+                            handleDeleteOptions={this.handleDeleteOptions}
+                            handleDeleteOption={this.handleDeleteOption}/>
+                        <AddOption 
+                            handleAddOption={this.handleAddOption} />
+                    </div>
+                    <OptionModal 
+                        selectedOption={this.state.selectedOption}
+                        handleClearSelectedOption = {this.handleClearSelectedOption}/>
+                </div>
             </div>
         )
     }
