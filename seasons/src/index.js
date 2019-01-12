7993c57ca0079431186625268fd532aa575b0f2d
dudelis <fukszon.kostya@gmail.com>
2019-01-11 17:28:56 -0800

    Played with states, conditional loading

---
+++ seasons/src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SeasonDisplay from './SeasonDisplay'
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {lat: null, errorMessage: '' };
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) =>{
+                this.setState({lat: position.coords.latitude});
+            },
+            (err) =>{
+                this.setState({errorMessage: err.message})
+            }
+        );
+
+    }
+
+    render(){
+        if (this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        if (!this.state.errorMessage && this.state.lat){
+            return <div>Latitude: {this.state.lat}</div>
+        }
+        return <div>Loading</div>
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
